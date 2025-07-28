32a68fa40ae0439c3116ecebab7dad364e6fdf35
hazelstoops <hazelstoops@gmail.com>
2025-07-27 23:37:35 -0700

    alter API path for Azure

---
+++ Hazel.Assessment.Web.UI/hazel.assessment.web.ui.client/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://kshackernews-dotazure-web-westus-dev-001-a3fddxfgendpfuaa.westus-01.azurewebsites.net'
+};
--- Hazel.Assessment.Web.UI/hazel.assessment.web.ui.client/src/proxy.conf.js
+++ Hazel.Assessment.Web.UI/hazel.assessment.web.ui.client/src/proxy.conf.js
@@ -1,7 +1,10 @@
 const { env } = require('process');
+import { environment } from './environments/environment';
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5022';
+//const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5022';
+
+const target = environment.apiUrl;
 
 const PROXY_CONFIG = [
   {
@@ -14,7 +17,7 @@ const PROXY_CONFIG = [
   },
   {
     "/*": {
-      "target": "https://localhost:5022/",
+      "target": target,
       "secure": false,
       "changeOrigin": true
     }

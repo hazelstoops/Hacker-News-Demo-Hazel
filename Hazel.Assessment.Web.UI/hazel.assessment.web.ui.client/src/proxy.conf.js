0dd36710e4d74099a824855dbeea5b6b491c7e5f
hazelstoops <hazelstoops@gmail.com>
2025-07-28 01:08:07 -0700

    restore local testing and re-added README

--- Hazel.Assessment.Web.UI/hazel.assessment.web.ui.client/src/proxy.conf.js
+++ Hazel.Assessment.Web.UI/hazel.assessment.web.ui.client/src/proxy.conf.js
@@ -1,16 +1,15 @@
 const { env } = require('process');
-import { environment } from './environments/environment';
+//import { environment } from './environments/environment';
 
-//const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-//  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5022';
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5022';
 
-const target = environment.apiUrl;
+//const target = environment.apiUrl;
 
 const PROXY_CONFIG = [
   {
     context: [
-      "/story",
-      "/teststory"
+      "/story"
     ],
     target,
     secure: false
@@ -23,7 +22,5 @@ const PROXY_CONFIG = [
     }
   }
 ]
-
-
 
 module.exports = PROXY_CONFIG;
